f625edd645a31eef5c4446773687903b6d6c2fcf
Evan <siyeel04@gmail.com>
2024-07-18 17:39:42 +0800

    Applied Animations and Connected Logic with Visuals

--- scripts/dom_module.js
+++ scripts/dom_module.js
@@ -1,6 +1,10 @@
 function onLoad() {
     const el_playerCard = document.querySelectorAll('.card-player');
-    const el_startButton = document.querySelector('.start-game'); 
+    const el_startButton = document.querySelector('.start-game');
+    const el_statusVictory = document.querySelectorAll('.container-status>.victory_count');
+    const el_statusMarker = document.querySelectorAll('.container-status>.marker');
+    const el_statusDefeat = document.querySelectorAll('.container-status>.defeat_count');
+    const wrapper_message = document.querySelectorAll('.wrapper-message');
 
     window.addEventListener('load', () => {
         el_playerCard.forEach(card => {
@@ -11,7 +15,7 @@ function onLoad() {
                 card.classList.remove('opacity-0');
                 card.classList.remove('slide-in_right');
             }, 3000);
-        }); 
+        });
 
         el_startButton.classList.add('slide-in_up');
         setTimeout(() => {
@@ -19,13 +23,118 @@ function onLoad() {
             el_startButton.classList.remove('slide-in_up');
         }, 3270);
     });
+
+    el_statusVictory.forEach(victory => {
+        victory.addEventListener('mouseover', () => {
+            wrapper_message.forEach(message => {
+                message.setAttribute('style', 'left: 16.5%;');
+                message.querySelector('.message').textContent = 'Victory(s)'
+            });
+        });
+    });
+
+    el_statusMarker.forEach(marker => {
+        marker.addEventListener('mouseover', () => {
+            wrapper_message.forEach(message => {
+                message.setAttribute('style', 'left: 50%;');
+                message.querySelector('.message').textContent = 'Marker'
+            });
+        });
+    });
+
+    el_statusDefeat.forEach(defeat => {
+        defeat.addEventListener('mouseover', () => {
+            wrapper_message.forEach(message => {
+                message.setAttribute('style', 'left: 83.5%;');
+                message.querySelector('.message').textContent = 'Defeat(s)'
+            });
+        });
+    });
+};
+
+function startGameAnimation() {
+    const el_gameScreen = document.querySelector('.container-game_screen');
+    const el_overlay = document.querySelector('.overlay');
+    const el_roundCounter = document.querySelector('.round');
+    const el_tile = document.querySelectorAll('.container-board .tile');
+    const el_backButton = document.querySelector('.container-back_reset .back');
+    const el_resetButton = document.querySelector('.container-back_reset .reset');
+    const el_playerCard1 = document.querySelector('.container-player_status_card #player1');
+    const el_playerCard2 = document.querySelector('.container-player_status_card #player2');
+
+
+
+    el_overlay.classList.remove('d-none')
+    el_gameScreen.classList.remove('d-none');
+    el_roundCounter.classList.remove('d-none');
+    el_roundCounter.classList.add('zoom-out_pop');
+    setTimeout(() => {
+        el_roundCounter.classList.remove('zoom-out_pop');
+    }, 500);
+
+    setTimeout(() => {
+        document.getElementById('tile-1').classList.add('border-top-left-radius');
+        document.getElementById('tile-3').classList.add('border-top-right-radius');
+        document.getElementById('tile-7').classList.add('border-bottom-left-radius');
+        document.getElementById('tile-9').classList.add('border-bottom-right-radius');
+
+        el_tile.forEach(tile => {
+            tile.classList.remove('d-none');
+            tile.classList.add('initial-box');
+            tile.classList.add('zoom-out_pop');
+            setTimeout(() => {
+                document.getElementById('tile-1').classList.remove('border-top-left-radius');
+                document.getElementById('tile-3').classList.remove('border-top-right-radius');
+                document.getElementById('tile-7').classList.remove('border-bottom-left-radius');
+                document.getElementById('tile-9').classList.remove('border-bottom-right-radius');
+                tile.classList.remove('initial-box');
+                tile.classList.remove('zoom-out_pop');
+            }, 1500);
+        });
+    }, 500);
+
+    setTimeout(() => {
+        el_backButton.classList.remove('d-none');
+        el_backButton.classList.add('zoom-out_pop');
+        setTimeout(() => {
+            el_backButton.classList.remove('zoom-out_pop');
+        }, 500);
+    }, 800);
+
+    setTimeout(() => {
+        el_resetButton.classList.remove('d-none');
+        el_resetButton.classList.add('zoom-out_pop');
+        setTimeout(() => {
+            el_resetButton.classList.remove('zoom-out_pop');
+        }, 500);
+    }, 1100);
+
+    setTimeout(() => {
+        el_playerCard1.classList.remove('d-none');
+        el_playerCard1.classList.add('zoom-out_pop');
+        setTimeout(() => {
+            el_playerCard1.classList.remove('zoom-out_pop');
+        }, 500);
+    }, 1400);
+
+    setTimeout(() => {
+        el_playerCard2.classList.remove('d-none');
+        el_playerCard2.classList.add('zoom-out_pop');
+        setTimeout(() => {
+            el_playerCard2.classList.remove('zoom-out_pop');
+        }, 500);
+    }, 1700);
+
+    setTimeout(() => {
+        el_overlay.classList.add('d-none');
+    }, 2000);
 };
 
 const startInterface = function () {
+    const el_titleScreen = document.querySelector('.container-title_screen');
     const el_mainTitle = document.querySelector('.container-title_screen .title');
     const el_startContainer = document.querySelectorAll('.container-title_screen>section')
-    console.log(el_mainTitle);
-    const el_playerCard = document.querySelectorAll('.card-player');
+    const el_playerCard = document.querySelectorAll('.container-player_section>.card-player');
     const el_startButton = document.querySelector('.start-game');
     let player1, player2;
 
@@ -64,7 +173,7 @@ const startInterface = function () {
                 card.classList.remove('zoom-in-pop');
                 card.classList.add('d-none');
             }, 2000);
-        });      
+        });
 
         setTimeout(() => {
             el_mainTitle.classList.add('zoom-in-pop');
@@ -72,7 +181,7 @@ const startInterface = function () {
         setTimeout(() => {
             el_mainTitle.classList.remove('zoom-in-pop');
         }, 1100);
-        
+
         setTimeout(() => {
             el_startButton.classList.add('zoom-in-pop');
         }, 900);
@@ -87,6 +196,13 @@ const startInterface = function () {
                 container.classList.add('d-none');
             }, 2000);
         });
+
+        setTimeout(() => {
+            el_titleScreen.classList.add('d-none');
+            startGameAnimation();
+        }, 2000);
+
+        Game();
     });
 
     const getStartEl = () => el_playerCard;
@@ -100,8 +216,64 @@ const startInterface = function () {
     }
 }();
 
-const startAnimation = function () {
+const gameInterface = function () {
+    const el_tile = document.querySelectorAll('.tile');
+    const el_playerCard = document.querySelectorAll('.container-player_status_card>.card-player')
+    const el_round = document.querySelector('.round');
+    const tiles = [];
+    const cards = [];
+    let round = 1;
 
+    el_tile.forEach(tile => {
+        tiles.push(tile);
+    });
+    
+    el_playerCard.forEach(card => {
+        cards.push(card);
+    });
+
+    const getTile = () => tiles;
+    const getPlayerCard = () => cards;
+    const nextRound = () => {
+        ++round;
+        el_round.textContent = `Round ${round}`;
+        el_tile.forEach(tile => {
+            tile.classList.remove('active');
+            tile.textContent = '';
+        });
+    };
+    const resetGameInterface = () => {
+        round = 1;
+        el_round.textContent = `Round ${round}`;
+
+        el_tile.forEach(tile => {
+            tile.classList.remove('active');
+            tile.textContent = '';
+        });
+
+        el_playerCard.forEach(card => {
+            const victory = card.querySelector('.victory_count');
+            const defeat = card.querySelector('.defeat_count');
+
+            victory.textContent = '0';
+            defeat.textContent = '0';
+        });
+    };
+
+    return {
+        getTile,
+        getPlayerCard,
+        nextRound,
+        resetGameInterface
+    }
+}();
+
+const playerTurn = function () {
+    const currentPlayer = (player) => player;
+
+    return {
+        currentPlayer
+    }
 }();
 
 onLoad();
--- scripts/game.js
+++ scripts/game.js
@@ -15,15 +15,18 @@ function Player(playername, marker) {
 };
 
 
-function Gameboard() {
+const gameboard = function () {
     const row = 3, col = 3;
     const board = [];
+    const visualBoard = gameInterface.getTile();
 
     const createBoard = () => {
+        let k = 0;
         for (let i = 0; i < row; ++i) {
             board.push(new Array(3))
             for (let j = 0; j < col; ++j) {
-                board[i].splice(j, 1, 0);
+                board[i].splice(j, 1, visualBoard[k]);
+                ++k;
             }
         };
     };
@@ -31,8 +34,8 @@ function Gameboard() {
     const getBoard = () => board;
     const resetBoard = () => board = [];
 
-    const addMarkOnBoard = (marker, row, col) => {
-        board[row - 1].splice(col - 1, 1, marker);
+    const addMarkOnBoard = (marker, tile) => {
+        tile.textContent = marker;
     };
 
     return {
@@ -41,7 +44,7 @@ function Gameboard() {
         resetBoard,
         addMarkOnBoard
     }
-};
+}();
 
 
 function Game() {
@@ -49,23 +52,33 @@ function Game() {
         return player.getUsername();
     };
 
-    function validateResponse(row, col) {
-        const boardArr = gameboard.getBoard();
-        if (row == 0 || col == 0) return false;
-
-        return boardArr[row - 1][col - 1] == 0;
+    function validateTile(tile) {
+        return tile.textContent !== '';
     };
 
-    function validateWinPattern(marker) {
-        const boardArr = gameboard.getBoard();
-        const length = boardArr.length;
+    function isDraw(board) {
+        const length = board.length;
+        let tiles = 0;
+        for (let i = 0; i < length; ++i) {
+            for (let j = 0; j < length; ++j) {
+                if (board[i][j].textContent !== '') {
+                    if (++tiles === 9) return true;
+                };
+            };
+        };
+
+        return false;
+    };
+
+    function validateWinPattern(board, marker) {
+        const length = board.length;
         let patternCount = 0;
 
         // Horizontal Pattern
         for (let i = 0; i < length; ++i) {
             patternCount = 0;
             for (let j = 0; j < length; ++j) {
-                if (boardArr[i][j] == marker) {
+                if (board[i][j].textContent === marker) {
                     patternCount++;
                     if (patternCount == 3) return true;
                 };
@@ -75,8 +88,9 @@ function Game() {
         patternCount = 0;
         // Vertical Pattern
         for (let i = 0; i < length; ++i) {
+            patternCount = 0;
             for (let j = 0; j < length; ++j) {
-                if (boardArr[j][i] == marker) {
+                if (board[j][i].textContent === marker) {
                     patternCount++;
                     if (patternCount == 3) return true;
                 };
@@ -86,7 +100,7 @@ function Game() {
         patternCount = 0;
         // Diagonal Left to Right
         for (let i = 0; i < length; ++i) {
-            if (boardArr[i][i] == marker) {
+            if (board[i][i].textContent === marker) {
                 patternCount++;
                 if (patternCount == 3) return true;
             };
@@ -95,7 +109,7 @@ function Game() {
         patternCount = 0;
         // Diagonal Right to Left
         for (let i = 1; i <= length; ++i) {
-            if (boardArr.at((i * -1)).at((i * -1)) == marker) {
+            if (board[i - 1].at(i * -1).textContent === marker) {
                 patternCount++;
                 if (patternCount == 3) return true;
             };
@@ -104,39 +118,219 @@ function Game() {
         return false;
     };
 
-    const gameboard = Gameboard();
-    gameboard.createBoard();
+    function changeCard(newCard, oldCard) {
+        newCard.classList.add('active');
+        newCard.querySelector('.container-profile').classList.add('active');
+        newCard.querySelector('.victory_count').classList.add('active');
+        newCard.querySelector('.marker').classList.add('active');
+        newCard.querySelector('.defeat_count').classList.add('active');
+        oldCard.classList.remove('active');
+        oldCard.querySelector('.container-profile').classList.remove('active');
+        oldCard.querySelector('.victory_count').classList.remove('active');
+        oldCard.querySelector('.marker').classList.remove('active');
+        oldCard.querySelector('.defeat_count').classList.remove('active');
+    }
 
-    const player1 = startInterface.getPlayer1();
-    const player2 = startInterface.getPlayer2();
+    function changePlayer() {
+        currentPlayer = currentPlayer === player1 ? player2 : player1;
+        marker = currentPlayer.getMarker();
 
-    let currentPlayer = player1, winner, row, col;
-    let showBoard = gameboard.getBoard();
-
-    while (true) {
-        let player = currentPlayer.getUsername();
-        let marker = currentPlayer.getMarker();
-        
-        console.log(`It's ${player}'s turn!`);
-        while (true) {
-            row = prompt('Please enter a row: ');
-            col = prompt('Please enter a col: ');
-            if (validateResponse(row, col)) break;
-            else alert('Please enter a valid position');
-        }
-
-        gameboard.addMarkOnBoard(marker, row, col);
-        console.log(showBoard[0]); console.log(showBoard[1]); console.log(showBoard[2]);
-        if (validateWinPattern(marker)) {
-            winner = getWinner(currentPlayer); 
-            console.log(`${winner}[${marker}] won!`); break;
-        }
-        
-        currentPlayer = currentPlayer == player1 ? player2 : player1;
+        if (currentPlayer === player1) {
+            changeCard(card1, card2);
+        } else {
+            changeCard(card2, card1);
+        };
     };
 
-    console.log(showBoard[0]); console.log(showBoard[1]); console.log(showBoard[2]);
+    // Initialization of gameboard
+    gameboard.createBoard();
+    let board = gameboard.getBoard();
+    
+    // Initialization of players
+    const player1 = startInterface.getPlayer1()
+    const player2 = startInterface.getPlayer2();
+
+    // Initialization of player cards
+    const card1 = gameInterface.getPlayerCard()[0];
+    const card2 = gameInterface.getPlayerCard()[1];
+    
+    // Initialization of current player
+    let currentPlayer = player1;
+
+    // Initialization of marker of the current player
+    let marker = currentPlayer.getMarker();
+    
+    // Adding username to player cards
+    card1.querySelector('.player').textContent = player1.getUsername();
+    card2.querySelector('.player').textContent = player2.getUsername();
+
+    // Initialize winner variable
+    let winner;
+
+    /*
+        After initial animation of game interface,
+        visually provide hint who is the first to play.
+    */
+    setTimeout(() => {
+        card1.classList.add('active');
+        card1.querySelector('.container-profile').classList.add('active');
+        card1.querySelector('.victory_count').classList.add('active');
+        card1.querySelector('.marker').classList.add('active');
+        card1.querySelector('.defeat_count').classList.add('active');
+    }, 4000);
+
+    /*
+        For each tile, add a listener when a player
+        inputs their marker and verify game flow.
+    */
+    for (let iter = 0; iter < board.length; ++iter) {
+        for (let jiter = 0; jiter < board.length; ++jiter) {
+            let tile = board[iter][jiter];
+
+            tile.addEventListener('mouseover', () => {
+                tile.classList.add('hover');
+            });
+            
+            tile.addEventListener('mousedown', () => {
+                tile.classList.add('active')
+            });
+            
+            tile.addEventListener('mouseleave', () => {
+                if (tile.textContent === '') {
+                    tile.classList.remove('active');
+                };
+                tile.classList.remove('hover');
+            });
+            
+            tile.addEventListener('mouseup', () => {
+                if (validateTile(tile)) {
+                    console.log('Marked');
+                    return;
+                };
+
+                gameboard.addMarkOnBoard(marker, tile);
+                tile.classList.add('active');
+                if (validateWinPattern(board, marker)) {
+                    console.log('WIN');
+                    winner = getWinner(currentPlayer);
+                    gameInterface.nextRound();
+                    return;
+                };
+
+                if (isDraw(board)) {
+                    console.log('Draw');
+                    return;
+                };
+
+                changePlayer();
+            });
+        };
+    };
+
+    const el_resetButton = document.querySelector('button.reset');
+    el_resetButton.addEventListener('mouseup', () => {
+        gameInterface.resetGameInterface();
+        // Reinitialization current player and marker
+        currentPlayer = player1;
+        marker = currentPlayer.getMarker();
+        changeCard(card1, card2);
+    });
 };
 
 // Game();
 // No global variables
+
+
+// function Game() {
+//     function getWinner(player) {
+//         return player.getUsername();
+//     };
+
+//     function validateResponse(row, col) {
+//         const boardArr = gameboard.getBoard();
+//         if (row == 0 || col == 0) return false;
+
+//         return boardArr[row - 1][col - 1] == 0;
+//     };
+
+//     function validateWinPattern(marker) {
+//         const boardArr = gameboard.getBoard();
+//         const length = boardArr.length;
+//         let patternCount = 0;
+
+//         // Horizontal Pattern
+//         for (let i = 0; i < length; ++i) {
+//             patternCount = 0;
+//             for (let j = 0; j < length; ++j) {
+//                 if (boardArr[i][j] == marker) {
+//                     patternCount++;
+//                     if (patternCount == 3) return true;
+//                 };
+//             };
+//         };
+
+//         patternCount = 0;
+//         // Vertical Pattern
+//         for (let i = 0; i < length; ++i) {
+//             for (let j = 0; j < length; ++j) {
+//                 if (boardArr[j][i] == marker) {
+//                     patternCount++;
+//                     if (patternCount == 3) return true;
+//                 };
+//             };
+//         };
+
+//         patternCount = 0;
+//         // Diagonal Left to Right
+//         for (let i = 0; i < length; ++i) {
+//             if (boardArr[i][i] == marker) {
+//                 patternCount++;
+//                 if (patternCount == 3) return true;
+//             };
+//         };
+        
+//         patternCount = 0;
+//         // Diagonal Right to Left
+//         for (let i = 1; i <= length; ++i) {
+//             if (boardArr.at((i * -1)).at((i * -1)) == marker) {
+//                 patternCount++;
+//                 if (patternCount == 3) return true;
+//             };
+//         };
+
+//         return false;
+//     };
+
+//     const gameboard = Gameboard();
+//     gameboard.createBoard();
+
+//     const player1 = startInterface.getPlayer1();
+//     const player2 = startInterface.getPlayer2();
+
+//     let currentPlayer = player1, winner, row, col;
+//     let showBoard = gameboard.getBoard();
+
+//     while (true) {
+//         let player = currentPlayer.getUsername();
+//         let marker = currentPlayer.getMarker();
+        
+//         console.log(`It's ${player}'s turn!`);
+//         while (true) {
+//             row = prompt('Please enter a row: ');
+//             col = prompt('Please enter a col: ');
+//             if (validateResponse(row, col)) break;
+//             else alert('Please enter a valid position');
+//         }
+
+//         gameboard.addMarkOnBoard(marker, row, col);
+//         console.log(showBoard[0]); console.log(showBoard[1]); console.log(showBoard[2]);
+//         if (validateWinPattern(marker)) {
+//             winner = getWinner(currentPlayer); 
+//             console.log(`${winner}[${marker}] won!`); break;
+//         }
+        
+//         currentPlayer = currentPlayer == player1 ? player2 : player1;
+//     };
+
+//     console.log(showBoard[0]); console.log(showBoard[1]); console.log(showBoard[2]);
+// };
