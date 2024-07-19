103f9f7de04aa3c9c93c5b6b8fe0d371fc78661a
Evan <siyeel04@gmail.com>
2024-07-19 18:03:33 +0800

    Finished Project
    All is left is chasing bugs.

--- scripts/dom_module.js
+++ scripts/dom_module.js
@@ -1,145 +1,4 @@
-function onLoad() {
-    const el_overlay = document.querySelector('.overlay')
-    const el_playerCard = document.querySelectorAll('.card-player');
-    const el_startButton = document.querySelector('.start-game');
-    const el_statusVictory = document.querySelectorAll('.container-status>.victory_count');
-    const el_statusMarker = document.querySelectorAll('.container-status>.marker');
-    const el_statusDefeat = document.querySelectorAll('.container-status>.defeat_count');
-    const wrapper_message = document.querySelectorAll('.wrapper-message');
-
-    window.addEventListener('load', () => {
-        el_overlay.classList.remove('d-none');
-        setTimeout(() => {
-            el_overlay.classList.add('d-none');
-        }, 3300);
-
-        el_playerCard.forEach(card => {
-            setTimeout(() => {
-                card.classList.add('slide-in_right');
-            }, 600);
-            setTimeout(() => {
-                card.classList.remove('opacity-0');
-                card.classList.remove('slide-in_right');
-            }, 3000);
-        });
-
-        el_startButton.classList.add('slide-in_up');
-        setTimeout(() => {
-            el_startButton.classList.remove('opacity-0');
-            el_startButton.classList.remove('slide-in_up');
-        }, 3270);
-    });
-
-    el_statusVictory.forEach(victory => {
-        victory.addEventListener('mouseover', () => {
-            wrapper_message.forEach(message => {
-                message.setAttribute('style', 'left: 16.5%;');
-                message.querySelector('.message').textContent = 'Victory(s)'
-            });
-        });
-    });
-
-    el_statusMarker.forEach(marker => {
-        marker.addEventListener('mouseover', () => {
-            wrapper_message.forEach(message => {
-                message.setAttribute('style', 'left: 50%;');
-                message.querySelector('.message').textContent = 'Marker'
-            });
-        });
-    });
-
-    el_statusDefeat.forEach(defeat => {
-        defeat.addEventListener('mouseover', () => {
-            wrapper_message.forEach(message => {
-                message.setAttribute('style', 'left: 83.5%;');
-                message.querySelector('.message').textContent = 'Defeat(s)'
-            });
-        });
-    });
-};
-
-function startGameAnimation() {
-    const el_gameScreen = document.querySelector('.container-game_screen');
-    const el_overlay = document.querySelector('.overlay');
-    const el_roundCounter = document.querySelector('.round');
-    const el_tile = document.querySelectorAll('.container-board .tile');
-    const el_backButton = document.querySelector('.container-back_reset .back');
-    const el_resetButton = document.querySelector('.container-back_reset .reset');
-    const el_playerCard1 = document.querySelector('.container-player_status_card #player1');
-    const el_playerCard2 = document.querySelector('.container-player_status_card #player2');
-
-
-
-    el_overlay.classList.remove('d-none')
-    el_gameScreen.classList.remove('d-none');
-    el_roundCounter.classList.remove('d-none');
-    el_roundCounter.classList.add('zoom-out_pop');
-    setTimeout(() => {
-        el_roundCounter.classList.remove('zoom-out_pop');
-    }, 500);
-
-    setTimeout(() => {
-        document.getElementById('tile-1').classList.add('border-top-left-radius');
-        document.getElementById('tile-3').classList.add('border-top-right-radius');
-        document.getElementById('tile-7').classList.add('border-bottom-left-radius');
-        document.getElementById('tile-9').classList.add('border-bottom-right-radius');
-
-        el_tile.forEach(tile => {
-            tile.classList.remove('d-none');
-            tile.classList.add('initial-box');
-            tile.classList.add('zoom-out_pop');
-            setTimeout(() => {
-                document.getElementById('tile-1').classList.remove('border-top-left-radius');
-                document.getElementById('tile-3').classList.remove('border-top-right-radius');
-                document.getElementById('tile-7').classList.remove('border-bottom-left-radius');
-                document.getElementById('tile-9').classList.remove('border-bottom-right-radius');
-                tile.classList.remove('initial-box');
-                tile.classList.remove('zoom-out_pop');
-            }, 1500);
-        });
-    }, 500);
-
-    setTimeout(() => {
-        el_backButton.classList.remove('d-none');
-        el_backButton.classList.add('zoom-out_pop');
-        setTimeout(() => {
-            el_backButton.classList.remove('zoom-out_pop');
-        }, 500);
-    }, 800);
-
-    setTimeout(() => {
-        el_resetButton.classList.remove('d-none');
-        el_resetButton.classList.add('zoom-out_pop');
-        setTimeout(() => {
-            el_resetButton.classList.remove('zoom-out_pop');
-        }, 500);
-    }, 1100);
-
-    setTimeout(() => {
-        el_playerCard1.classList.remove('d-none');
-        el_playerCard1.classList.add('zoom-out_pop');
-        setTimeout(() => {
-            el_playerCard1.classList.remove('zoom-out_pop');
-        }, 500);
-    }, 1400);
-
-    setTimeout(() => {
-        el_playerCard2.classList.remove('d-none');
-        el_playerCard2.classList.add('zoom-out_pop');
-        setTimeout(() => {
-            el_playerCard2.classList.remove('zoom-out_pop');
-        }, 500);
-    }, 1700);
-
-    setTimeout(() => {
-        el_overlay.classList.add('d-none');
-    }, 2000);
-};
-
 const startInterface = function () {
-    const el_titleScreen = document.querySelector('.container-title_screen');
-    const el_mainTitle = document.querySelector('.container-title_screen .title');
-    const el_startContainer = document.querySelectorAll('.container-title_screen>section')
     const el_playerCard = document.querySelectorAll('.container-player_section>.card-player');
     const el_startButton = document.querySelector('.start-game');
     let player1, player2;
@@ -164,51 +23,30 @@ const startInterface = function () {
     });
 
     el_startButton.addEventListener('mouseup', () => {
-        let input1 = document.querySelector('#player1');
-        let input2 = document.querySelector('#player2');
+        let input1 = document.querySelector('.container-player_section #player1');
+        let input2 = document.querySelector('.container-player_section #player2');
 
         input1 = input1.value !== '' ? input1.value : 'Player 1';
         input2 = input2.value !== '' ? input2.value : 'Player 2';
 
+        console.log(input1);
+        console.log(input2);
+
         const playerCard1 = gameInterface.getPlayerCard(0);
         const playerCard2 = gameInterface.getPlayerCard(1);
+
+        console.log(playerCard1);
+        console.log(playerCard2);
 
         player1 = Player(input1, 'X', playerCard1);
         player2 = Player(input2, 'O', playerCard2);
 
-        el_playerCard.forEach(card => {
-            card.classList.add('zoom-in-pop');
-            setTimeout(() => {
-                card.classList.remove('zoom-in-pop');
-                card.classList.add('d-none');
-            }, 2000);
-        });
+        console.log(player1);
+        console.log(player2);
 
+        animationLoad.unloadStartScreen();
         setTimeout(() => {
-            el_mainTitle.classList.add('zoom-in-pop');
-        }, 500);
-        setTimeout(() => {
-            el_mainTitle.classList.remove('zoom-in-pop');
-        }, 1100);
-
-        setTimeout(() => {
-            el_startButton.classList.add('zoom-in-pop');
-        }, 900);
-        setTimeout(() => {
-            el_mainTitle.classList.add('d-none');
-            el_startButton.classList.remove('zoom-in-pop');
-            el_startButton.classList.add('d-none');
-        }, 1900);
-
-        el_startContainer.forEach(container => {
-            setTimeout(() => {
-                container.classList.add('d-none');
-            }, 2000);
-        });
-
-        setTimeout(() => {
-            el_titleScreen.classList.add('d-none');
-            startGameAnimation();
+            animationLoad.loadGameScreen()
         }, 2000);
 
         Game();
@@ -239,6 +77,7 @@ const gameInterface = function () {
     el_playerCard.forEach(card => {
         cards.push(card);
     });
+
 
     const getTile = () => tiles;
     const getPlayerCard = (index) => cards[index];
@@ -323,7 +162,7 @@ const gameInterface = function () {
             });
         }, 4400);
     };
-    
+
     const resetGameInterface = () => {
         if (round > 1) {
             round = 1;
@@ -363,12 +202,266 @@ const gameInterface = function () {
     }
 }();
 
-const playerTurn = function () {
-    const currentPlayer = (player) => player;
+const animationLoad = function () {
+    const el_titleScreen = document.querySelector('.container-title_screen');
+    const el_overlay = document.querySelector('.overlay')
+    const el_title = document.querySelector('.container-title');
+    const el_mainTitle = document.querySelector('.container-title_screen .title');
+    const el_playerCard = document.querySelectorAll('.container-player_section>.card-player');
+    const el_startButton = document.querySelector('.start-game');
+    const el_statusVictory = document.querySelectorAll('.container-status>.victory_count');
+    const el_statusMarker = document.querySelectorAll('.container-status>.marker');
+    const el_statusDefeat = document.querySelectorAll('.container-status>.defeat_count');
+    const wrapper_message = document.querySelectorAll('.wrapper-message');
+
+    // Initial load
+    window.addEventListener('load', () => {
+        loadStartScreen();
+
+        el_statusVictory.forEach(victory => {
+            victory.addEventListener('mouseover', () => {
+                wrapper_message.forEach(message => {
+                    message.setAttribute('style', 'left: 16.5%;');
+                    message.querySelector('.message').textContent = 'Victory(s)'
+                });
+            });
+        });
+
+        el_statusMarker.forEach(marker => {
+            marker.addEventListener('mouseover', () => {
+                wrapper_message.forEach(message => {
+                    message.setAttribute('style', 'left: 50%;');
+                    message.querySelector('.message').textContent = 'Marker'
+                });
+            });
+        });
+
+        el_statusDefeat.forEach(defeat => {
+            defeat.addEventListener('mouseover', () => {
+                wrapper_message.forEach(message => {
+                    message.setAttribute('style', 'left: 83.5%;');
+                    message.querySelector('.message').textContent = 'Defeat(s)'
+                });
+            });
+        });
+    });
+
+    const loadStartScreen = () => {
+        el_titleScreen.classList.remove('d-none')
+        el_overlay.classList.remove('d-none');
+        el_title.classList.remove('d-none');
+
+        setTimeout(() => {
+            el_overlay.classList.add('d-none');
+        }, 3300);
+
+        el_mainTitle.classList.remove('d-none');
+
+        el_playerCard.forEach(card => {
+            setTimeout(() => {
+                card.classList.remove('d-none');
+            }, 1);
+            card.classList.add('opacity-0')
+            setTimeout(() => {
+                card.classList.add('slide-in_right');
+            }, 600);
+            setTimeout(() => {
+                card.classList.remove('opacity-0');
+                card.classList.remove('slide-in_right');
+            }, 3000);
+        });
+
+        el_startButton.classList.add('slide-in_up');
+        setTimeout(() => {
+            el_startButton.classList.remove('d-none');
+        }, 2000);
+        setTimeout(() => {
+            el_overlay.classList.add('d-none');
+            el_startButton.classList.remove('opacity-0');
+            el_startButton.classList.remove('slide-in_up');
+        }, 3000);
+    }
+
+    const unloadStartScreen = () => {
+        // const el_startContainer = document.querySelectorAll('.container-title_screen>section')
+        const el_playerCard = document.querySelectorAll('.container-player_section>.card-player');
+
+        el_overlay.classList.remove('d-none');
+        el_playerCard.forEach(card => {
+            card.classList.add('zoom-in-pop');
+            setTimeout(() => {
+                card.classList.remove('zoom-in-pop');
+                card.classList.add('d-none');
+            }, 2000);
+        });
+
+        setTimeout(() => {
+            el_mainTitle.classList.add('zoom-in-pop');
+        }, 500);
+        setTimeout(() => {
+            el_mainTitle.classList.remove('zoom-in-pop');
+        }, 1100);
+
+        setTimeout(() => {
+            el_startButton.classList.add('zoom-in-pop');
+        }, 900);
+        setTimeout(() => {
+            el_mainTitle.classList.add('d-none');
+            el_startButton.classList.remove('zoom-in-pop');
+            el_startButton.classList.add('d-none');
+        }, 1900);
+
+        // el_startContainer.forEach(container => {
+        //     setTimeout(() => {
+        //         container.classList.add('d-none');
+        //     }, 2000);
+        // });
+
+        setTimeout(() => {
+            el_titleScreen.classList.add('d-none');
+        }, 2000);
+    };
+
+    const loadGameScreen = () => {
+        const el_gameScreen = document.querySelector('.container-game_screen');
+        const el_roundCounter = document.querySelector('.round');
+        const el_tile = document.querySelectorAll('.container-board .tile');
+        const el_backButton = document.querySelector('.container-back_reset .back');
+        const el_resetButton = document.querySelector('.container-back_reset .reset');
+        const el_playerCard1 = document.querySelector('.container-player_status_card #player1');
+        const el_playerCard2 = document.querySelector('.container-player_status_card #player2');
+
+        el_gameScreen.classList.remove('d-none');
+        el_roundCounter.classList.remove('d-none');
+        el_roundCounter.classList.add('zoom-out_pop');
+        setTimeout(() => {
+            el_roundCounter.classList.remove('zoom-out_pop');
+        }, 500);
+
+        setTimeout(() => {
+            document.getElementById('tile-1').classList.add('border-top-left-radius');
+            document.getElementById('tile-3').classList.add('border-top-right-radius');
+            document.getElementById('tile-7').classList.add('border-bottom-left-radius');
+            document.getElementById('tile-9').classList.add('border-bottom-right-radius');
+
+            el_tile.forEach(tile => {
+                tile.classList.remove('d-none');
+                tile.classList.add('initial-box');
+                tile.classList.add('zoom-out_pop');
+                setTimeout(() => {
+                    document.getElementById('tile-1').classList.remove('border-top-left-radius');
+                    document.getElementById('tile-3').classList.remove('border-top-right-radius');
+                    document.getElementById('tile-7').classList.remove('border-bottom-left-radius');
+                    document.getElementById('tile-9').classList.remove('border-bottom-right-radius');
+                    tile.classList.remove('initial-box');
+                    tile.classList.remove('zoom-out_pop');
+                }, 1500);
+            });
+        }, 500);
+
+        setTimeout(() => {
+            el_backButton.classList.remove('d-none');
+            el_backButton.classList.add('zoom-out_pop');
+            setTimeout(() => {
+                el_backButton.classList.remove('zoom-out_pop');
+            }, 500);
+        }, 800);
+
+        setTimeout(() => {
+            el_resetButton.classList.remove('d-none');
+            el_resetButton.classList.add('zoom-out_pop');
+            setTimeout(() => {
+                el_resetButton.classList.remove('zoom-out_pop');
+            }, 500);
+        }, 1100);
+
+        setTimeout(() => {
+            el_playerCard1.classList.remove('d-none');
+            el_playerCard1.classList.add('zoom-out_pop');
+            setTimeout(() => {
+                el_playerCard1.classList.remove('zoom-out_pop');
+            }, 500);
+        }, 1400);
+
+        setTimeout(() => {
+            el_playerCard2.classList.remove('d-none');
+            el_playerCard2.classList.add('zoom-out_pop');
+            setTimeout(() => {
+                el_playerCard2.classList.remove('zoom-out_pop');
+            }, 500);
+        }, 1700);
+
+        setTimeout(() => {
+            el_overlay.classList.add('d-none');
+        }, 2000);
+    };
+
+    const unloadGameScreen = () => {
+        const el_gameScreen = document.querySelector('.container-game_screen');
+        const el_roundCounter = document.querySelector('.round');
+        const el_tile = document.querySelectorAll('.container-board .tile');
+        const el_backButton = document.querySelector('.container-back_reset .back');
+        const el_resetButton = document.querySelector('.container-back_reset .reset');
+        const el_playerCard1 = document.querySelector('.container-player_status_card #player1');
+        const el_playerCard2 = document.querySelector('.container-player_status_card #player2');
+
+        el_playerCard2.classList.add('zoom-in-pop');
+        setTimeout(() => {
+            el_playerCard2.classList.remove('zoom-in-pop');
+            el_playerCard2.classList.add('d-none');
+        }, 1000);
+
+        setTimeout(() => {
+            el_playerCard1.classList.add('zoom-in-pop');
+            setTimeout(() => {
+                el_playerCard1.classList.remove('zoom-in-pop');
+                el_playerCard1.classList.add('d-none');
+            }, 1000);
+        }, 500);
+
+        setTimeout(() => {
+            el_resetButton.classList.add('zoom-in-pop');
+            setTimeout(() => {
+                el_resetButton.classList.remove('zoom-in-pop');
+                el_resetButton.classList.add('d-none');
+            }, 1000);
+        }, 1000);
+
+        setTimeout(() => {
+            el_backButton.classList.add('zoom-in-pop');
+            setTimeout(() => {
+                el_backButton.classList.remove('zoom-in-pop');
+                el_backButton.classList.add('d-none');
+            }, 1000);
+        }, 1500);
+
+        setTimeout(() => {
+            el_tile.forEach(tile => {
+                tile.classList.add('zoom-in-pop');
+                setTimeout(() => {
+                    tile.classList.remove('zoom-in-pop');
+                    tile.classList.add('d-none');
+                }, 1000);
+            });
+        }, 2000);
+
+        setTimeout(() => {
+            el_roundCounter.classList.add('zoom-in-pop');
+            setTimeout(() => {
+                el_roundCounter.classList.remove('zoom-in-pop');
+                el_roundCounter.classList.add('d-none');
+            }, 1000);
+        }, 2500);
+
+        setTimeout(() => {
+            el_gameScreen.classList.add('d-none');
+        }, 3000);
+    };
 
     return {
-        currentPlayer
+        loadStartScreen,
+        unloadStartScreen,
+        loadGameScreen,
+        unloadGameScreen
     }
 }();
-
-onLoad();
--- scripts/game.js
+++ scripts/game.js
@@ -257,102 +257,12 @@ function Game() {
         marker = currentPlayer.getMarker();
         changeCard(card1, card2);
     });
+
+    const el_backButton = document.querySelector('button.back');
+    el_backButton.addEventListener('mouseup', () => {
+        animationLoad.unloadGameScreen();
+        setTimeout(() => {
+            animationLoad.loadStartScreen();
+        }, 3500);
+    });
 };
-
-// Game();
-// No global variables
-
-
-// function Game() {
-//     function getWinner(player) {
-//         return player.getUsername();
-//     };
-
-//     function validateResponse(row, col) {
-//         const boardArr = gameboard.getBoard();
-//         if (row == 0 || col == 0) return false;
-
-//         return boardArr[row - 1][col - 1] == 0;
-//     };
-
-//     function validateWinPattern(marker) {
-//         const boardArr = gameboard.getBoard();
-//         const length = boardArr.length;
-//         let patternCount = 0;
-
-//         // Horizontal Pattern
-//         for (let i = 0; i < length; ++i) {
-//             patternCount = 0;
-//             for (let j = 0; j < length; ++j) {
-//                 if (boardArr[i][j] == marker) {
-//                     patternCount++;
-//                     if (patternCount == 3) return true;
-//                 };
-//             };
-//         };
-
-//         patternCount = 0;
-//         // Vertical Pattern
-//         for (let i = 0; i < length; ++i) {
-//             for (let j = 0; j < length; ++j) {
-//                 if (boardArr[j][i] == marker) {
-//                     patternCount++;
-//                     if (patternCount == 3) return true;
-//                 };
-//             };
-//         };
-
-//         patternCount = 0;
-//         // Diagonal Left to Right
-//         for (let i = 0; i < length; ++i) {
-//             if (boardArr[i][i] == marker) {
-//                 patternCount++;
-//                 if (patternCount == 3) return true;
-//             };
-//         };
-        
-//         patternCount = 0;
-//         // Diagonal Right to Left
-//         for (let i = 1; i <= length; ++i) {
-//             if (boardArr.at((i * -1)).at((i * -1)) == marker) {
-//                 patternCount++;
-//                 if (patternCount == 3) return true;
-//             };
-//         };
-
-//         return false;
-//     };
-
-//     const gameboard = Gameboard();
-//     gameboard.createBoard();
-
-//     const player1 = startInterface.getPlayer1();
-//     const player2 = startInterface.getPlayer2();
-
-//     let currentPlayer = player1, winner, row, col;
-//     let showBoard = gameboard.getBoard();
-
-//     while (true) {
-//         let player = currentPlayer.getUsername();
-//         let marker = currentPlayer.getMarker();
-        
-//         console.log(`It's ${player}'s turn!`);
-//         while (true) {
-//             row = prompt('Please enter a row: ');
-//             col = prompt('Please enter a col: ');
-//             if (validateResponse(row, col)) break;
-//             else alert('Please enter a valid position');
-//         }
-
-//         gameboard.addMarkOnBoard(marker, row, col);
-//         console.log(showBoard[0]); console.log(showBoard[1]); console.log(showBoard[2]);
-//         if (validateWinPattern(marker)) {
-//             winner = getWinner(currentPlayer); 
-//             console.log(`${winner}[${marker}] won!`); break;
-//         }
-        
-//         currentPlayer = currentPlayer == player1 ? player2 : player1;
-//     };
-
-//     console.log(showBoard[0]); console.log(showBoard[1]); console.log(showBoard[2]);
-// };
